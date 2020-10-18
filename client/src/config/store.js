d50a86b5b399a01034651bb96e79d50168d175f5
Afuza <theafuza@gmail.com>
2020-10-18 23:35:36 +0700

    added endpoint for local development

--- client/src/config/store.js
+++ client/src/config/store.js
@@ -4,13 +4,13 @@ import thunk from "redux-thunk";
 
 import allReducers from "../store/reducers";
 
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
   allReducers,
   // persistedState,
-  // composeEnhancers(applyMiddleware(thunk))
-  applyMiddleware(thunk)
+  composeEnhancers(applyMiddleware(thunk))
+  // applyMiddleware(thunk)
 );
 
 // store.subscribe(() => saveToLocalStorage(store.getState()));
